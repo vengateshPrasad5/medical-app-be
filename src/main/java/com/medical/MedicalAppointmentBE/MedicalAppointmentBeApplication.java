54fc451413fb8fd4fd9af407a7cc7e298d78976b
VengateshPrasad <venkuvenky2@gmail.com>
2024-05-23 11:59:31 +0530

    initial commit

---
+++ src/main/java/com/medical/MedicalAppointmentBE/MedicalAppointmentBeApplication.java
@@ -0,0 +1,13 @@
+package com.medical.MedicalAppointmentBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalAppointmentBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalAppointmentBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medical/MedicalAppointmentBE/MedicalAppointmentBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medical.MedicalAppointmentBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalAppointmentBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
